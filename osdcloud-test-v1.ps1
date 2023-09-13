f5332b3263b736205bfc19be68857e6409df8aa9
Peter Daalmans [MVP] <peter@daalmans.com>
2023-09-13 03:19:06 -0700

    osdcloud-test-v1.ps1

---
+++ osdcloud-test-v1.ps1
@@ -0,0 +1,92 @@
+#New file to test OSDCloud
+
+Write-Host -ForegroundColor Green “Starting OSDCloud ZTI”
+
+Start-Sleep -Seconds 5
+
+#Change Display Resolution for Virtual Machine
+
+if ((Get-MyComputerModel) -match ‘Virtual’) {
+
+Write-Host -ForegroundColor Green “Setting Display Resolution to 1600x”
+
+Set-DisRes 1600
+
+}
+
+#Make sure I have the latest OSD Content
+
+Write-Host -ForegroundColor Green “Updating OSD PowerShell Module”
+
+Install-Module OSD -Force
+
+Write-Host -ForegroundColor Green “Importing OSD PowerShell Module”
+
+Import-Module OSD -Force
+
+#Start OSDCloud ZTI the RIGHT way
+
+Write-Host -ForegroundColor Green “Start OSDCloud”
+
+Start-OSDCloud -OSLanguage en-us -OSBuild 22H2 -OSEdition Enterprise -ZTI
+
+#================================================
+#  [PostOS] OOBEDeploy Configuration
+#================================================
+Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json"
+$OOBEDeployJson = @'
+{
+    "AddNetFX3":  {
+                      "IsPresent":  true
+                  },
+    "Autopilot":  {
+                      "IsPresent":  false
+                  },
+    "RemoveAppx":  [
+                    "MicrosoftTeams",
+                    "Microsoft.BingWeather",
+                    "Microsoft.BingNews",
+                    "Microsoft.GamingApp",
+                    "Microsoft.GetHelp",
+                    "Microsoft.Getstarted",
+                    "Microsoft.Messaging",
+                    "Microsoft.MicrosoftOfficeHub",
+                    "Microsoft.MicrosoftSolitaireCollection",
+                    "Microsoft.MicrosoftStickyNotes",
+                    "Microsoft.MSPaint",
+                    "Microsoft.People",
+                    "Microsoft.PowerAutomateDesktop",
+                    "Microsoft.StorePurchaseApp",
+                    "Microsoft.Todos",
+                    "microsoft.windowscommunicationsapps",
+                    "Microsoft.WindowsFeedbackHub",
+                    "Microsoft.WindowsMaps",
+                    "Microsoft.WindowsSoundRecorder",
+                    "Microsoft.Xbox.TCUI",
+                    "Microsoft.XboxGameOverlay",
+                    "Microsoft.XboxGamingOverlay",
+                    "Microsoft.XboxIdentityProvider",
+                    "Microsoft.XboxSpeechToTextOverlay",
+                    "Microsoft.YourPhone",
+                    "Microsoft.ZuneMusic",
+                    "Microsoft.ZuneVideo"
+                   ],
+    "UpdateDrivers":  {
+                          "IsPresent":  true
+                      },
+    "UpdateWindows":  {
+                          "IsPresent":  true
+                      }
+}
+'@
+If (!(Test-Path "C:\ProgramData\OSDeploy")) {
+    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
+}
+$OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
+
+#=======================================================================
+#   Restart-Computer
+#=======================================================================
+Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
